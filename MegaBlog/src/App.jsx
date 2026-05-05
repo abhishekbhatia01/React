3140688b8c6e1fc5ac02c76180d9286a327f1ce7
Abhishek Bhatia <abhibhatia0410@gmail.com>
2026-05-05 17:38:55 +0530

    feat: add initial project structure with components, assets, and configuration
    - Added SVG assets for React and Vite.
    - Created reusable Button and Container components.
    - Implemented Footer and Header components with navigation.
    - Added Logout button functionality with Redux integration.
    - Created Input component for form handling.
    - Established a Logo component for branding.
    - Set up Redux store and authentication slice for user management.
    - Configured Vite with React and Tailwind CSS support.
    - Added global CSS file for Tailwind imports.
    - Created configuration file for Appwrite integration.

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logOut } from './store/authSlice';
+import { Footer, Header } from './components';
+
+function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}));
+      }else{
+        dispatch(logOut());
+      }
+    })
+    .finally(() => setLoading(false));
+  }, []);
+
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODD: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
+}
+
+export default App
